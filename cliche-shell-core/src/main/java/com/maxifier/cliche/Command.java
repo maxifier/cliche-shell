ce2dd6e224bb9b90fff8ccade1ffd571874ac9da
Igor Yankov <igor.yankov@maxifier.com>
2015-01-20 20:14:23 +0400

    Add completer to command so that auto-completing jline logic can be defined for each command

--- cliche-shell-core/src/main/java/com/maxifier/cliche/Command.java
+++ cliche-shell-core/src/main/java/com/maxifier/cliche/Command.java
@@ -5,13 +5,17 @@
 
 package com.maxifier.cliche;
 
+import jline.console.completer.Completer;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.List;
 
 /**
  * Annotation for commands. Allows to specify the name of a command, otherwise method's name is used.
+ *
  * @author ASG
  */
 @Target(ElementType.METHOD)
@@ -19,6 +23,7 @@ import java.lang.annotation.Target;
 public @interface Command {
     /**
      * Allows to override default command name, which is derived from method's name
+     *
      * @return "" or null if default name is used, user-specified name otherwise.
      */
     String name() default ""; // if "" then Null is assumed.
@@ -26,6 +31,7 @@ public @interface Command {
     /**
      * Specify the description of the command. Default description (if this
      * property is not set) says "methodName(Arg1Type, Arg2Type,...) : ReturnType".
+     *
      * @return command's description or "" if not set.
      */
     String description() default "";
@@ -34,13 +40,30 @@ public @interface Command {
      * Specify the shortcut name for the command.
      * If not set, if the name attribute is not set as well, the Shell takes
      * the first letter of each word (void selectUser() --- select-user --- su).
+     *
      * @return command's abbreviation or "" if not set.
      */
     String abbrev() default "";
 
     /**
      * Specify the string to output before command's output, i.e. some explanations.
+     *
      * @return command's header or "" if not set.
      */
     String header() default "";
+
+    /**
+     * Specify the Class which will implement auto-complete logic. The class should return list of possible options by terms.
+     *
+     * @return command's header or "" if not set.
+     */
+    Class<? extends Completer> completer() default DEFAULT_COMPLETER.class;
+
+    static final class DEFAULT_COMPLETER implements Completer {
+        @Override
+        public int complete(String buffer, int cursor, List<CharSequence> candidates) {
+            return 0;
+        }
+    }
+
 }
